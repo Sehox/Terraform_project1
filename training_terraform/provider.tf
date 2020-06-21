82651a798c98c400caf05e3be2dd61cbb1672d77
user1 <ahmed.sehnani@gmail.com>
2020-06-21 20:39:54 +0200

    removing 0.11 syntax

--- training_terraform/provider.tf
+++ training_terraform/provider.tf
@@ -1,9 +1,9 @@
 provider "aws" {
-  region = "${var.region}"
+  region = var.region
 }
 resource "aws_vpc" "main" {
 
-  cidr_block = "${var.vpc_cidr}"
+  cidr_block = var.vpc_cidr
   instance_tenancy = "default"
   tags = {
     Name = "Main"
@@ -11,9 +11,9 @@ resource "aws_vpc" "main" {
   }
 }
 resource "aws_subnet" "subnets" {
-  count = "${length(var.azs)}"
-  cidr_block = "${element(var.subnet_cidr,count.index )}"
-  vpc_id = "${aws_vpc.main.id}"
+  count = length(var.azs)
+  cidr_block = element(var.subnet_cidr,count.index )
+  vpc_id = aws_vpc.main.id
   tags = {
     Name = "Subnet1"
 
--- training_terraform/vars.tf
+++ training_terraform/vars.tf
@@ -8,12 +8,12 @@ variable "vpc_cidr" {
   default = "190.160.0.0/16"
 }
 variable "subnet_cidr" {
-  type = "list"
+  type = list
   default = ["192.168.1.0/24", "190.160.2.0/24", "190.160.3.0/24"]
 
 }
 
 variable "azs" {
-  type = "list"
+  type = list
   default = ["eu-west-3a", "eu-west-3b", "eu-west-3c"]
 }
