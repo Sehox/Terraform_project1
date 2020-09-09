53652013265674253e6c75b65139657757bf20f5
SEHNANI, Ahmed Amine (NEUSTA) <ahmed-amine.sehnani.external@airbus.com>
2020-09-09 10:39:31 +0200

    same

--- environments/int/outputs.tf
+++ environments/int/outputs.tf
@@ -61,6 +61,6 @@ output "alb_alias_name" {
 
 output "private_alb_hosted_zone_id" {
     value       = aws_route53_zone.private.zone_id
-    value       = module.ro.route53_private_zone_with_alb_alias.zone_id
+    value       = module.route53_private_zone_with_alb_alias.zone_id
     description = "private_alb_hosted_zone ID"
 }
